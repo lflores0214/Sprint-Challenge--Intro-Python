0c25ca5adef545c90d89bffd3f49b772697d64c4
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-13 08:25:50 -0700

    initial commit

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,13 +24,13 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [d for d in humans if d.name[0] == "D" ]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [e for e in humans if e.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
@@ -41,20 +41,20 @@ print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [d for d in humans if d.age > 1 (d.age + 10) ]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [e for e in humans if e (f"{e.name}-{e.age}")]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [f for f in humans if f.age > 27 and f.age < 32 (f.name, f.age) ]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
