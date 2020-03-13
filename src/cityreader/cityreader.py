f720f60972006520d86de6dc728454d90ee8f309
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-13 10:11:08 -0700

    done with oop2 done with cityreader.py done with mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,22 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  
+  # def __str__(self):
+  #   return str(vars(self))
+  def __str__(self):
+    s = f"{self.name}, {float(self.lat)}, {float(self.lon)}"
+    return s
+import csv
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -20,7 +31,12 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv', 'r') as city_file:
+    reader = csv.reader(city_file)
+    next(reader)
+    for column in reader:
+        city = City(column[0],float(column[3]),float(column[4]))
+        cities.append(city)
     return cities
 
 cityreader(cities)
